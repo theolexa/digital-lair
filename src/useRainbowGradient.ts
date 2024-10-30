c8f693f1492b123db92e0164e3cf74cd158551ed
theolexa <khomenkoalexandr@gmail.com>
2024-10-30 22:29:21 +0200

    vertical gradient

--- src/useRainbowGradient.ts
+++ src/useRainbowGradient.ts
@@ -42,7 +42,7 @@ function hslToRgb(h: number, s: number, l: number) {
     return `rgb(${r}, ${g}, ${b})`
   }
   
-  // get list of rgb collors
+// get list of rgb collors
 function generateRainbowGradient(seed: number, numColors: number) {
     const rgbColors = [];
     const hueStep = 1 / numColors;
@@ -65,13 +65,59 @@ function generateRainbowGradient(seed: number, numColors: number) {
     }
       return rgbColors;
   }
+
+function generateRainbowGradient2(seed: number, numLines: number, numColors: number) {
+    const rgbColors = []
+    const hueStep = 1 / (numColors - 1); // Adjusted for zero-based indexing
   
-  // html ASCII text & styles
-  function bunnerColorsMap(text: string, colors: []) {
+    for (let line = 0; line < numLines; line++) {
+      const lineHue = (seed + line * hueStep) % 1; // Calculate hue for the current line
+      const lineColors = []
+  
+      for (let i = 0; i < numColors; i++) {  
+        const hue = (lineHue + i * hueStep) % 1;
+        const saturation = 1;
+        const lightness = 0.5;
+  
+        const hslColor = `hsl(${hue * 360}, ${saturation * 100}%, ${lightness * 100}%)`;
+        const match = hslColor.match(/\d+(\.\d+)?/g);
+  
+        if (match) {
+          const [h, s, l] = match.map(Number);
+          lineColors.push(hslToRgb(h, s, l));
+        } else {
+          console.error("Error parsing HSL color:", hslColor);
+        }
+      }
+      rgbColors.push(lineColors)
+    }
+  
+    return rgbColors;
+  }
+
+// html ASCII text & styles
+function bunnerColorsMap(text: string, colors: []) {
     const htmlCss = []
   
     for (let i = 0; i < text.length; i++) {
       htmlCss.push(`<span style={{color: "${colors[i]}"}}>${text[i]}</span>`)
     }
     return htmlCss.join('')
+  }
+
+function bunnerColorsMap2(textLines: [string], colorLines: [string]) {
+    const htmlCss = []
+
+    for (let i = 0; i < textLines.length; i++) {
+      const linehtmlCss = []
+      const currentTextLine = textLines[i]
+      const currentColorLines = colorLines[i]
+      for (let j = 0; j < currentTextLine.length; j++) {
+        linehtmlCss.push(`<span style={{color: "${currentColorLines[j]}"}} data-text="${currentTextLine[j]}"><span>${currentTextLine[j]}</span></span>`)
+      }
+      htmlCss.push('<div>' + linehtmlCss.join('') + '</div>')
+
+    }
+    return htmlCss.join('')
+   
   }
